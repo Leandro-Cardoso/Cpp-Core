07e6f4cadfbd9c3bcdd1e4a4a6e49de6d7956505
Leandro Cardoso <leandrocardosodev@gmail.com>
2026-02-12 23:12:53 -0300

    Ponteiro em estruturas e objetos

--- ponteiros.cpp
+++ ponteiros.cpp
@@ -3,6 +3,13 @@
 
 using namespace std;
 
+struct Pessoa
+{
+    int id = 1;
+    int idade = 36;
+};
+
+
 int main() {
     int numero = 5;
 
@@ -16,6 +23,14 @@ int main() {
     // Acessando informações do ponteiro:
     cout << endl << "Posição de memoria do ponteiro: " << ponteiro << endl;
     cout << "Valor: " << *ponteiro << endl;
+
+    // Com objetos e estruturas:
+    Pessoa pessoa = Pessoa();
+    cout << endl << "ID: " << pessoa.id << endl << "IDADE: " << pessoa.idade << endl;
+
+    // Com objetos e estruturas (ponteiro):
+    Pessoa *pessoaPonteiro = &pessoa;
+    cout << endl << "ID: " << pessoaPonteiro->id << endl << "IDADE: " << pessoaPonteiro->idade << endl;
 
     return 0;
 }
