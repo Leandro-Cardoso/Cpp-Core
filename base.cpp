57de5c979d8dfe2e19be7148f3dba30ffba33939
Leandro Cardoso <leandrocardosodev@gmail.com>
2026-02-20 23:25:44 -0300

    Saida e entrada de informacao

--- base.cpp
+++ base.cpp
@@ -4,9 +4,14 @@ using namespace std; // Indicação da biblioteca padrão.
 
 // Função principal e onde o programa inicia (retorna int).
 int main() {
-    cout << endl << "Ola, mundo !!!" << endl << endl;
+    string nome;
 
-    system("pause"); // Pausa o programa.
+    cout << endl << "Nome: "; // Saida de informacao.
+    cin >> nome; // Entrada de informacao.
+
+    cout << endl << endl << "Ola, " << nome << " !!!" << endl << endl;
+
+    //system("pause"); // Pausa o programa.
 
     return 0; // Final do programa.
 }
