034db61b0d4e787e3d38b06b08e1a89f2d61142d
Leandro Cardoso <leandrocardosodev@gmail.com>
2026-02-20 21:29:50 -0300

    Base

---
+++ README.md
@@ -0,0 +1,6 @@
+# C++ Core
+
+## Basico:
+
+* [000 - Base](https://github.com/Leandro-Cardoso/Cpp-Core/blob/main/base.cpp)
+* [001 - Base]()
---
+++ base.cpp
@@ -0,0 +1,12 @@
+#include <iostream> // Comandos base de saida e entrada.
+
+using namespace std; // Indicação da biblioteca padrão.
+
+// Função principal e onde o programa inicia (retorna int).
+int main() {
+    cout << endl << "Ola, mundo !!!" << endl << endl;
+
+    system("pause"); // Pausa o programa.
+
+    return 0; // Final do programa.
+}
