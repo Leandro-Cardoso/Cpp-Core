afbad08f0eb2af43b0c042a8efbc5414fafd12cd
Leandro Cardoso <leandrocardosodev@gmail.com>
2026-02-21 23:08:37 -0300

    Operadores basicos

---
+++ operadores.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Mesma regra da matematica na ordem de resolucao.
+
+    cout << endl << "OPERADORES:" << endl;
+
+    // Soma:
+    cout << "\t10 + 5 = " << 10 + 5 << " (soma)" << endl;
+
+    // Subtracao:
+    cout << "\t10 - 5 = " << 10 + 5 << " (subtracao)" << endl;
+
+    // Multiplicacao:
+    cout << "\t10 * 5 = " << 10 * 5 << " (multiplicacao)" << endl;
+
+    // Divisao:
+    cout << "\t10 / 5 = " << 10 / 5 << "  (divisao)" << endl;
+
+    // Resto da divisao:
+    cout << "\t10 % 5 = " << 10 % 5 << "  (resto da divisao)" << endl;
+
+    return 0;
+}
