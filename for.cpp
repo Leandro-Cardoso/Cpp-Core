c66f217458f5aabb28384d182505be0fc28e92b9
Leandro Cardoso <leandrocardosodev@gmail.com>
2026-03-09 22:37:37 -0300

    For

---
+++ for.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int opcao;
+
+    cout << endl << "Digite um numero inteiro: ";
+    cin >> opcao;
+    cout << "Voce digitou '" << opcao << "' !!!" << endl;
+
+    for (int i = 0; i == opcao; ++i) {
+        cout << endl << i;
+    }
+
+    cout << endl;
+
+    return 0;
+}
